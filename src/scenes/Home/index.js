985008b2a8467aca792ddd07317b3cf9763450dd
Jacob Armentrout <jacob.armentrout@veeva.com>
2018-06-09 10:38:59 -0700

    fixing image paths

--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -8,32 +8,32 @@ export default class Home extends React.Component {
             <div>
                 <PageHeader>Software/DevOps Engineer</PageHeader>
                 <Row className="show-grid row-no-padding flex-row">
-                    <Tile src="/portrait.jpg" title="Jacob Armentrout (me!)" subtitle1="Software Engineer. I love creating solutions." subtitle2="Where there's a will, there's a way"/>
-                    <Tile src="/wd.jpg" title="Western Digital (3yr)" subtitle1="Technician + Testing Engineer Internship" subtitle2="Hard drive testing + automation" href="https://www.wdc.com/" external/>
-                    <Tile src="/hgst.png" title="HGST (summer)" subtitle1="Software Engineer Internship" subtitle2="SVN diff notifier" href="https://www.hgst.com/" external/>
-                    <Tile src="/uci_cs.jpg" title="UCI CS (1yr)" subtitle1="Student Linux Administrator" subtitle2="Server maintenance + puppet development" href="https://www.ics.uci.edu/computing/index.php" external/>
-                    <Tile src="/veeva.jpg" title="Veeva (1yr+, Working Here!)" subtitle1="Associate Software Engineer" subtitle2="Built DevOps python library, Jenkins automation in AWS, Ansible provisioning, etc." href="https://www.veeva.com/" external/>
+                    <Tile src="https://github.com/SirJakesalot/jacob-armentrout-website/blob/master/public/portrait.jpg" title="Jacob Armentrout (me!)" subtitle1="Software Engineer. I love creating solutions." subtitle2="Where there's a will, there's a way"/>
+                    <Tile src="https://github.com/SirJakesalot/jacob-armentrout-website/blob/master/public/wd.jpg" title="Western Digital (3yr)" subtitle1="Technician + Testing Engineer Internship" subtitle2="Hard drive testing + automation" href="https://www.wdc.com/" external/>
+                    <Tile src="https://github.com/SirJakesalot/jacob-armentrout-website/blob/master/public/hgst.png" title="HGST (summer)" subtitle1="Software Engineer Internship" subtitle2="SVN diff notifier" href="https://www.hgst.com/" external/>
+                    <Tile src="https://github.com/SirJakesalot/jacob-armentrout-website/blob/master/public/uci_cs.jpg" title="UCI CS (1yr)" subtitle1="Student Linux Administrator" subtitle2="Server maintenance + puppet development" href="https://www.ics.uci.edu/computing/index.php" external/>
+                    <Tile src="https://github.com/SirJakesalot/jacob-armentrout-website/blob/master/public/veeva.jpg" title="Veeva (1yr+, Working Here!)" subtitle1="Associate Software Engineer" subtitle2="Built DevOps python library, Jenkins automation in AWS, Ansible provisioning, etc." href="https://www.veeva.com/" external/>
                 </Row>
                 <PageHeader>Image Gallery About Me</PageHeader>
                 <Row className="show-grid row-no-padding flex-row">
-                    <Image src="/bamboo_forest_stream.jpg" alt="No image found" />
-                    <Image src="/Bell.jpg" alt="No image found" />
-                    <Image src="/big_ben.jpg" alt="No image found" />
-                    <Image src="/boardgames.jpg" alt="No image found" />
-                    <Image src="/makena_landing.jpg" alt="No image found" />
-                    <Image src="/fallout4.jpg" alt="No image found" />
-                    <Image src="/foosball.jpg" alt="No image found" />
-                    <Image src="/Guinness.jpg" alt="No image found" />
-                    <Image src="/jakes_tutorials.jpg" alt="No image found" />
-                    <Image src="/kittens.jpg" alt="No image found" />
-                    <Image src="/escape_room.jpeg" alt="No image found" />
-                    <Image src="/munchkins.jpg" alt="No image found" />
-                    <Image src="/ping_pong.jpg" alt="No image found" />
-                    <Image src="/raspberry_pi.jpg" alt="No image found" />
-                    <Image src="/skyrim.jpg" alt="No image found" />
-                    <Image src="/Turtle.JPG" alt="No image found" />
-                    <Image src="/uci.png" alt="No image found" />
-                    <Image src="/xmas_tree.jpg" alt="No image found" />
+                    <Image src="https://github.com/SirJakesalot/jacob-armentrout-website/blob/master/public/bamboo_forest_stream.jpg" alt="No image found" />
+                    <Image src="https://github.com/SirJakesalot/jacob-armentrout-website/blob/master/public/Bell.jpg" alt="No image found" />
+                    <Image src="https://github.com/SirJakesalot/jacob-armentrout-website/blob/master/public/big_ben.jpg" alt="No image found" />
+                    <Image src="https://github.com/SirJakesalot/jacob-armentrout-website/blob/master/public/boardgames.jpg" alt="No image found" />
+                    <Image src="https://github.com/SirJakesalot/jacob-armentrout-website/blob/master/public/makena_landing.jpg" alt="No image found" />
+                    <Image src="https://github.com/SirJakesalot/jacob-armentrout-website/blob/master/public/fallout4.jpg" alt="No image found" />
+                    <Image src="https://github.com/SirJakesalot/jacob-armentrout-website/blob/master/public/foosball.jpg" alt="No image found" />
+                    <Image src="https://github.com/SirJakesalot/jacob-armentrout-website/blob/master/public/Guinness.jpg" alt="No image found" />
+                    <Image src="https://github.com/SirJakesalot/jacob-armentrout-website/blob/master/public/jakes_tutorials.jpg" alt="No image found" />
+                    <Image src="https://github.com/SirJakesalot/jacob-armentrout-website/blob/master/public/kittens.jpg" alt="No image found" />
+                    <Image src="https://github.com/SirJakesalot/jacob-armentrout-website/blob/master/public/escape_room.jpeg" alt="No image found" />
+                    <Image src="https://github.com/SirJakesalot/jacob-armentrout-website/blob/master/public/munchkins.jpg" alt="No image found" />
+                    <Image src="https://github.com/SirJakesalot/jacob-armentrout-website/blob/master/public/ping_pong.jpg" alt="No image found" />
+                    <Image src="https://github.com/SirJakesalot/jacob-armentrout-website/blob/master/public/raspberry_pi.jpg" alt="No image found" />
+                    <Image src="https://github.com/SirJakesalot/jacob-armentrout-website/blob/master/public/skyrim.jpg" alt="No image found" />
+                    <Image src="https://github.com/SirJakesalot/jacob-armentrout-website/blob/master/public/Turtle.JPG" alt="No image found" />
+                    <Image src="https://github.com/SirJakesalot/jacob-armentrout-website/blob/master/public/uci.png" alt="No image found" />
+                    <Image src="https://github.com/SirJakesalot/jacob-armentrout-website/blob/master/public/xmas_tree.jpg" alt="No image found" />
                 </Row>
                 <br />
             </div>
